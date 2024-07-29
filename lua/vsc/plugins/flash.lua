cc88e1f2eb494c10f7abf358864b4c9704ceef40
Your Name <you@example.com>
2024-07-29 17:28:43 +0800

    feat: update version and add comment for flash.nvim

--- lua/vsc/plugins/flash.lua
+++ lua/vsc/plugins/flash.lua
@@ -6,8 +6,8 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" }, -- use a jump label, or use ; and , to increase/decrease the selection
     -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
