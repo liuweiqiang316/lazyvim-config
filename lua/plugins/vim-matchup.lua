d1fe8f470a4eca2afd0c81b88564ffaee433a9cd
Your Name <you@example.com>
2024-11-19 19:02:44 +0800

    feat: update

--- lua/plugins/vim-matchup.lua
+++ lua/plugins/vim-matchup.lua
@@ -1,6 +1,10 @@
+if true then
+  return {}
+end
+
 return {
-    'andymass/vim-matchup',
-    setup = function()
-        vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    end,
+  "andymass/vim-matchup",
+  setup = function()
+    vim.g.matchup_matchparen_offscreen = { method = "popup" }
+  end,
 }
