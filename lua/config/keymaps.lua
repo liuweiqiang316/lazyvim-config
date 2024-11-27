902f36d18626ba7c3445e8e43b4461d9411feaf8
lwq <liuweiqiang316@163.com>
2024-11-27 09:55:09 +0800

    feat: update keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,42 +17,37 @@ map("n", "<C-k><C-n>", "<cmd>new<cr>", { desc = "New File" })
 
 map("i", "<C-v>", "<C-r>*", { desc = "map ctrl-v to paste" })
 
+map("i", "<C-n>", "<Right>", { desc = "move to Right" })
+map("i", "<C-p>", "<Left>", { desc = "move to Left" })
+map("i", "<C-S-n>", "<C-Right>", { desc = "move to Right word" })
+map("i", "<C-S-p>", "<C-Left>", { desc = "move to Left word" })
+
+vim.keymap.set("n", "<C-f>", "*")
+vim.keymap.set("v", "<C-f>", 'y/<C-R>"<CR>')
+
+vim.keymap.set("n", "<leader>df", "0f{V%d", { desc = "delete function" })
+vim.keymap.set("n", "<leader>db", "0V%d", { desc = "delete surround %" })
+
+vim.keymap.set("n", "<leader>[", "][%0w", { desc = "快速跳转到上一个函数名" })
+vim.keymap.set("n", "<leader>ts", "/setup<CR>", { desc = "goto vue setup script" })
+
 -- move
 vim.keymap.set("n", "<S-h>", "^", { desc = "Line header" })
 vim.keymap.set("n", "<S-l>", "$", { desc = "Line end" })
+vim.keymap.set("v", "<S-h>", "^", { desc = "Line header" })
+vim.keymap.set("v", "<S-l>", "$", { desc = "Line end" })
 
-vim.keymap.set(
-  "n",
-  "<S-e>",
-  "<Plug>(MatchitNormalBackward)",
-  { noremap = false, desc = "映射 shift + e 为matchup %" }
-)
-vim.keymap.set(
-  "n",
-  "<S-e>",
-  "<Plug>(MatchitNormalForward)",
-  { noremap = false, desc = "映射 shift + e 为matchup %" }
-)
-vim.keymap.set(
-  "v",
-  "<S-e>",
-  "<Plug>(MatchitNormalBackward)",
-  { noremap = false, desc = "映射 shift + e 为matchup %" }
-)
-vim.keymap.set(
-  "v",
-  "<S-e>",
-  "<Plug>(MatchitNormalForward)",
-  { noremap = false, desc = "映射 shift + e 为matchup %" }
-)
+for _, operator in pairs({ "d", "c", "y" }) do
+  vim.keymap.set("n", operator .. "H", operator .. "^")
+  vim.keymap.set("n", operator .. "L", operator .. "$")
+end
+
+vim.keymap.set("n", "<S-e>", "<Plug>(MatchitNormalBackward)", { noremap = false, desc = "map E to %" })
+vim.keymap.set("n", "<S-e>", "<Plug>(MatchitNormalForward)", { noremap = false, desc = "map E to %" })
+vim.keymap.set("v", "<S-e>", "<Plug>(MatchitVisualBackward)", { noremap = false, desc = "map E to %" })
+vim.keymap.set("v", "<S-e>", "<Plug>(MatchitVisualForward)", { noremap = false, desc = "map E to %" })
 
 vim.keymap.set("i", "<C-l>", "<Del>", { desc = "delete right char" })
-
--- move line head and end
-vim.keymap.set("n", "H", "^")
-vim.keymap.set("x", "H", "^")
-vim.keymap.set("n", "L", "$")
-vim.keymap.set("x", "L", "$")
 
 -- vim-visual-multi
 -- vim.keymap.set("n", "<C-A-k>", "<Plug>(VM-Add-Cursor-Up)", { desc = "向上添加光标" })
