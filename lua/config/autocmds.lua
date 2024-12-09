562dcb8e987c66d84436a3d01f8e3901befe4ff1
lwq <liuweiqiang316@163.com>
2024-12-09 11:14:26 +0800

    feat: 添加console autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -20,3 +20,24 @@
 --     print("👉", exeStr)
 --   end,
 -- })
+
+-- 定义一个函数来获取当前光标下的单词，并在下一行插入console.log语句
+local function insert_console_log()
+  local word = vim.fn.expand("<cword>")
+  local line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  -- 插入console.log语句到下一行
+  vim.api.nvim_buf_set_lines(0, line, line, false, { "console.log(" .. word .. ")" })
+  -- 确保光标移动到的位置不会超出缓冲区范围
+  local last_line = vim.api.nvim_buf_line_count(0)
+  vim.api.nvim_win_set_cursor(0, { math.min(line + 1, last_line), 0 })
+end
+
+-- 创建一个用户命令来调用 Lua 函数
+vim.api.nvim_create_user_command("InsertConsoleLog", insert_console_log, { nargs = 0 })
+
+-- 使用autocmd设置快捷键
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  -- pattern = { "vue", "javascript" },
+  pattern = "*",
+  command = "nnoremap <space>l <CMD>InsertConsoleLog<CR>",
+})
