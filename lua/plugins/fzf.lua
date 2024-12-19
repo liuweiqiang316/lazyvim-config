b359451b897a66973c36899bb32662ef0d8b5d23
lwq <liuweiqiang316@163.com>
2024-12-19 17:35:46 +0800

    feat: fzf-lua 添加grep相关keymap

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -11,6 +11,24 @@ return {
       desc = "Switch Opened Buffer",
     },
     { "<leader>;", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
+    { "<leader>sv", mode = { "n" }, "<cmd>FzfLua grep_cword<cr>", desc = "Grep Under Word" },
+    { "<leader>sv", mode = { "v" }, "<cmd>FzfLua grep_visual<cr>", desc = "Grep Visual Word" },
+    {
+      "<leader>/",
+      mode = { "n", "v" },
+      function()
+        local mode = vim.api.nvim_get_mode().mode
+        local word = vim.fn.expand("<cword>")
+        -- 将光标下的字符复制到寄存器w
+        vim.fn.setreg("w", word)
+        if mode == "v" then
+          -- 如果是visual模式，将选中的内容复制到寄存器w，用于后续<C-r>w的粘贴
+          vim.cmd('normal! "wy')
+        end
+        require("fzf-lua").live_grep()
+      end,
+      desc = "Grep (Root Dir)",
+    },
   },
   opts = function(_, opts)
     local actions = require("fzf-lua.actions")
@@ -24,6 +42,12 @@ return {
         preview = {
           horizontal = "right:40%",
         },
+        on_create = function()
+          vim.keymap.set("t", "<C-r>", [['<C-\><C-N>"'.nr2char(getchar()).'pi']], { expr = true, buffer = true })
+        end,
+        on_close = function()
+          vim.fn.setreg("w", "")
+        end,
       },
       grep = {
         -- prompt            = 'Rg❯ ',
