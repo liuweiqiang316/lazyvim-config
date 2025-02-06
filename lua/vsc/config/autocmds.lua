0d5ea2bca66f4d55e99c4360142129b0e3faefe8
Your Name <you@example.com>
2025-02-06 10:50:29 +0800

    bug: 修复vscode中使用类似ci'等text-object光标定位不准确问题

--- lua/vsc/config/autocmds.lua
+++ lua/vsc/config/autocmds.lua
@@ -2,20 +2,30 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- 用于normal模式和insert模式切换中英文
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-    pattern = "*",
-    command = "silent !C:\\greenSoft\\im-select.exe 2052 && C:\\im-select\\im-select.exe 1033",
-})
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-    pattern = "*",
-    command = "silent !C:\\greenSoft\\im-select.exe 1033 && C:\\im-select\\im-select.exe 2052",
-})
---
 vim.api.nvim_create_autocmd({ "CmdLineLeave" }, {
     pattern = "*",
     callback = function(event)
         local exeStr = vim.fn.getcmdline()
         print('👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇', exeStr)
     end
+})
+
+local function switchIm(imCode)
+    local str = "C:/greenSoft/im-select.exe"
+
+    vim.loop.spawn(str, {
+        args = { imCode },
+        detach = true,
+    })
+end
+
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+    callback = function()
+        switchIm("2052")
+    end,
+})
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    callback = function()
+        switchIm("1033")
+    end,
 })
